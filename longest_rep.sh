78507bc5c259b0ea5493ef83b34b0de4602e3b03
Marcel <70788314+msseguin@users.noreply.github.com>
2022-12-17 16:28:08 -0700

    fixed logic error for initial character count

--- longest_rep.sh
+++ longest_rep.sh
@@ -5,26 +5,30 @@ topLetter=''
 declare -i newCount=0
 string=$1
 
+# loop through all chars
 for (( i=0; i<${#string}; i++ ));
 do
   letter=${string:$i:1} 
-  echo "${newCount}"
-   
-  # check if it is the same as last character
-  if [[ "$letter" == "$lastLetter" ]] || [ "$newCount" -eq 0 ]
+
+  # check if it is not the same as last character, and set count to 1 if it is
+  if [[ "$letter" != "$lastLetter" ]]
   then
-    newCount=$(( newCount + 1))
+    newCount=1
     lastLetter=$letter
-    if [ "$newCount" -gt "$count" ]
+   else 
+   newCount=$(( newCount + 1))
+   lastLetter=$letter
+  fi
+  if [ "$newCount" -gt "$count" ]
     then
         count=$newCount
         topLetter=$letter
     fi
-   else
-    newCount=0
-  fi
 done
 
+
+
+# return the top repeated value
 echo "${topLetter},${count}"
 
 
